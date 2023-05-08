2577525cbec7bc3b35c4e80a02a32b405d619754
Jordan Masone <jordanmasone1@gmail.com>
2023-05-08 15:25:00 -0400

    Added the hold and next grids.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,30 @@ const populateGrid = async () => {
             await delay(10);
         }
     }
+
+    // Sub-Grids
+    for (let i = 0; i < 5; i++) {
+        const sectionOne = document.createElement("article");
+        sectionOne.setAttribute("id", `next-y${i}`);
+        sectionOne.setAttribute("class", "section");
+        document.getElementById("next").appendChild(sectionOne);
+
+        const sectionTwo = document.createElement("article");
+        sectionTwo.setAttribute("id", `hold-y${i}`);
+        sectionTwo.setAttribute("class", "section");
+        document.getElementById("hold").appendChild(sectionTwo);
+
+        for (let j = 0; j < 5; j++) {
+            const divOne = document.createElement("div");
+            divOne.setAttribute("class", "small-cell");
+            sectionOne.appendChild(divOne);
+
+            const divTwo = document.createElement("div");
+            divTwo.setAttribute("class", "small-cell");
+            sectionTwo.appendChild(divTwo);
+        }
+    }
+
 }
 
 populateGrid();
